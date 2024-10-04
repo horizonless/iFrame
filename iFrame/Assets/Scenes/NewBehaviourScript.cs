ae010746a69dea835206604583f8cd826555d234
L1ZI7 <zhangshenrui1017@gmail.com>
2024-10-04 17:25:33 +0800

    add uniwindow package

--- iFrame/Assets/Scenes/NewBehaviourScript.cs
+++ iFrame/Assets/Scenes/NewBehaviourScript.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Kirurobo;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    
+    {
+        UniWindowController.;
     }
 
     // Update is called once per frame
