f203d2a9406e3ad6494d98d28c8198b796b9f182
horizonless <zhangshenrui66@yeah.net>
2024-10-17 03:14:00 +0800

    finish part of church inside

--- iFrame/Assets/iFrame/Scripts/iFrameChurchInsideManager.cs
+++ iFrame/Assets/iFrame/Scripts/iFrameChurchInsideManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using SFB;
 using UnityEngine;
 
 public class iFrameChurchInsideManager : MonoBehaviour
@@ -7,8 +9,9 @@ public class iFrameChurchInsideManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        // StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false));
+        // StandaloneFileBrowser.OpenFolderPanel("Select Folder", Path.Combine(Application.streamingAssetsPath, "TheTruth"), true);
+        System.Diagnostics.Process.Start(Path.Combine(Application.streamingAssetsPath, "TheTruth"));
     }
 
     // Update is called once per frame
